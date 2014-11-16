940533e2e5874526554803183715c07c5667e7d8
Dhiana Deva <dhiana.deva@gmail.com>
2014-11-16 20:00:30 -0200

    Retab mna.cpp

--- src/mna.cpp
+++ src/mna.cpp
@@ -17,12 +17,12 @@ Versao 1.0i - 03/11/2013 Correcoes em *p e saida com sistema singular.
 Elementos aceitos e linhas do netlist:
 
 Resistor:  R<nome> <no+> <no-> <resistencia>
-VCCS:      G<nome> <io+> <io-> <vi+> <vi-> <transcondutancia>
-VCVC:      E<nome> <vo+> <vo-> <vi+> <vi-> <ganho de tensao>
-CCCS:      F<nome> <io+> <io-> <ii+> <ii-> <ganho de corrente>
-CCVS:      H<nome> <vo+> <vo-> <ii+> <ii-> <transresistencia>
-Fonte I:   I<nome> <io+> <io-> <corrente>
-Fonte V:   V<nome> <vo+> <vo-> <tensao>
+VCCS:            G<nome> <io+> <io-> <vi+> <vi-> <transcondutancia>
+VCVC:            E<nome> <vo+> <vo-> <vi+> <vi-> <ganho de tensao>
+CCCS:            F<nome> <io+> <io-> <ii+> <ii-> <ganho de corrente>
+CCVS:            H<nome> <vo+> <vo-> <ii+> <ii-> <transresistencia>
+Fonte I:     I<nome> <io+> <io-> <corrente>
+Fonte V:     V<nome> <vo+> <vo-> <tensao>
 Amp. op.:  O<nome> <vo1> <vo2> <vi1> <vi2>
 
 As fontes F e H tem o ramo de entrada em curto
@@ -49,278 +49,278 @@ using namespace std;
 #define DEBUG
 
 typedef struct elemento { /* Elemento do netlist */
-  char nome[MAX_NOME];
-  double valor;
-  int a,b,c,d,x,y;
+    char nome[MAX_NOME];
+    double valor;
+    int a,b,c,d,x,y;
 } elemento;
 
 elemento netlist[MAX_ELEM]; /* Netlist */
 
 int
-  ne, /* Elementos */
-  nv, /* Variaveis */
-  nn, /* Nos */
-  i,j,k;
+    ne, /* Elementos */
+    nv, /* Variaveis */
+    nn, /* Nos */
+    i,j,k;
 
 char
 /* Foram colocados limites nos formatos de leitura para alguma protecao
-   contra excesso de caracteres nestas variaveis */
-  nomearquivo[MAX_LINHA+1],
-  tipo,
-  na[MAX_NOME],nb[MAX_NOME],nc[MAX_NOME],nd[MAX_NOME],
-  lista[MAX_NOS+1][MAX_NOME+2], /*Tem que caber jx antes do nome */
-  txt[MAX_LINHA+1],
-  *p;
+     contra excesso de caracteres nestas variaveis */
+    nomearquivo[MAX_LINHA+1],
+    tipo,
+    na[MAX_NOME],nb[MAX_NOME],nc[MAX_NOME],nd[MAX_NOME],
+    lista[MAX_NOS+1][MAX_NOME+2], /*Tem que caber jx antes do nome */
+    txt[MAX_LINHA+1],
+    *p;
 FILE *arquivo;
 
 double
-  g,
-  Yn[MAX_NOS+1][MAX_NOS+2];
+    g,
+    Yn[MAX_NOS+1][MAX_NOS+2];
 
 /* Rotina que conta os nos e atribui numeros a eles */
 int numero(char *nome)
 {
-  int i,achou;
+    int i,achou;
 
-  i=0; achou=0;
-  while (!achou && i<=nv)
-    if (!(achou=!strcmp(nome,lista[i]))) i++;
-  if (!achou) {
-    if (nv==MAX_NOS) {
-      printf("O programa so aceita ate %d nos\n",nv);
-      exit(1);
+    i=0; achou=0;
+    while (!achou && i<=nv)
+        if (!(achou=!strcmp(nome,lista[i]))) i++;
+    if (!achou) {
+        if (nv==MAX_NOS) {
+            printf("O programa so aceita ate %d nos\n",nv);
+            exit(1);
+        }
+        nv++;
+        strcpy(lista[nv],nome);
+        return nv; /* novo no */
     }
-    nv++;
-    strcpy(lista[nv],nome);
-    return nv; /* novo no */
-  }
-  else {
-    return i; /* no ja conhecido */
-  }
+    else {
+        return i; /* no ja conhecido */
+    }
 }
 
 int main(void)
 {
-  system("clear");
-  printf("Programa demonstrativo de analise nodal modificada\n");
-  printf("Por Antonio Carlos M. de Queiroz - acmq@coe.ufrj.br\n");
-  printf("Versao %s\n",versao);
+    system("clear");
+    printf("Programa demonstrativo de analise nodal modificada\n");
+    printf("Por Antonio Carlos M. de Queiroz - acmq@coe.ufrj.br\n");
+    printf("Versao %s\n",versao);
  denovo:
-  /* Leitura do netlist */
-  ne=0; nv=0; strcpy(lista[0],"0");
-  printf("Nome do arquivo com o netlist (ex: mna.net): ");
-  scanf("%50s",nomearquivo);
-  arquivo=fopen(nomearquivo,"r");
+    /* Leitura do netlist */
+    ne=0; nv=0; strcpy(lista[0],"0");
+    printf("Nome do arquivo com o netlist (ex: mna.net): ");
+    scanf("%50s",nomearquivo);
+    arquivo=fopen(nomearquivo,"r");
 
-  if (arquivo==0) {
-    printf("Arquivo %s inexistente\n",nomearquivo);
-    goto denovo;
-  }
-  printf("Lendo netlist:\n");
-  fgets(txt,MAX_LINHA,arquivo);
-  printf("Titulo: %s",txt);
-  while (fgets(txt,MAX_LINHA,arquivo)) {
-    ne++; /* Nao usa o netlist[0] */
-    if (ne>MAX_ELEM) {
-      printf("O programa so aceita ate %d elementos\n",MAX_ELEM);
-      exit(1);
+    if (arquivo==0) {
+        printf("Arquivo %s inexistente\n",nomearquivo);
+        goto denovo;
     }
-    txt[0]=toupper(txt[0]);
-    tipo=txt[0];
-    sscanf(txt,"%10s",netlist[ne].nome);
-    p=txt+strlen(netlist[ne].nome); /* Inicio dos parametros */
-    /* O que e lido depende do tipo */
-    if (tipo=='R' || tipo=='I' || tipo=='V') {
-      sscanf(p,"%10s%10s%lg",na,nb,&netlist[ne].valor);
-      printf("%s %s %s %g\n",netlist[ne].nome,na,nb,netlist[ne].valor);
-      netlist[ne].a=numero(na);
-      netlist[ne].b=numero(nb);
+    printf("Lendo netlist:\n");
+    fgets(txt,MAX_LINHA,arquivo);
+    printf("Titulo: %s",txt);
+    while (fgets(txt,MAX_LINHA,arquivo)) {
+        ne++; /* Nao usa o netlist[0] */
+        if (ne>MAX_ELEM) {
+            printf("O programa so aceita ate %d elementos\n",MAX_ELEM);
+            exit(1);
+        }
+        txt[0]=toupper(txt[0]);
+        tipo=txt[0];
+        sscanf(txt,"%10s",netlist[ne].nome);
+        p=txt+strlen(netlist[ne].nome); /* Inicio dos parametros */
+        /* O que e lido depende do tipo */
+        if (tipo=='R' || tipo=='I' || tipo=='V') {
+            sscanf(p,"%10s%10s%lg",na,nb,&netlist[ne].valor);
+            printf("%s %s %s %g\n",netlist[ne].nome,na,nb,netlist[ne].valor);
+            netlist[ne].a=numero(na);
+            netlist[ne].b=numero(nb);
+        }
+        else if (tipo=='G' || tipo=='E' || tipo=='F' || tipo=='H') {
+            sscanf(p,"%10s%10s%10s%10s%lg",na,nb,nc,nd,&netlist[ne].valor);
+            printf("%s %s %s %s %s %g\n",netlist[ne].nome,na,nb,nc,nd,netlist[ne].valor);
+            netlist[ne].a=numero(na);
+            netlist[ne].b=numero(nb);
+            netlist[ne].c=numero(nc);
+            netlist[ne].d=numero(nd);
+        }
+        else if (tipo=='O') {
+            sscanf(p,"%10s%10s%10s%10s",na,nb,nc,nd);
+            printf("%s %s %s %s %s\n",netlist[ne].nome,na,nb,nc,nd);
+            netlist[ne].a=numero(na);
+            netlist[ne].b=numero(nb);
+            netlist[ne].c=numero(nc);
+            netlist[ne].d=numero(nd);
+        }
+        else if (tipo=='*') { /* Comentario comeca com "*" */
+            printf("Comentario: %s",txt);
+            ne--;
+        }
+        else {
+            printf("Elemento desconhecido: %s\n",txt);
+            cin.get();
+            exit(1);
+        }
     }
-    else if (tipo=='G' || tipo=='E' || tipo=='F' || tipo=='H') {
-      sscanf(p,"%10s%10s%10s%10s%lg",na,nb,nc,nd,&netlist[ne].valor);
-      printf("%s %s %s %s %s %g\n",netlist[ne].nome,na,nb,nc,nd,netlist[ne].valor);
-      netlist[ne].a=numero(na);
-      netlist[ne].b=numero(nb);
-      netlist[ne].c=numero(nc);
-      netlist[ne].d=numero(nd);
-    }
-    else if (tipo=='O') {
-      sscanf(p,"%10s%10s%10s%10s",na,nb,nc,nd);
-      printf("%s %s %s %s %s\n",netlist[ne].nome,na,nb,nc,nd);
-      netlist[ne].a=numero(na);
-      netlist[ne].b=numero(nb);
-      netlist[ne].c=numero(nc);
-      netlist[ne].d=numero(nd);
-    }
-    else if (tipo=='*') { /* Comentario comeca com "*" */
-      printf("Comentario: %s",txt);
-      ne--;
-    }
-    else {
-      printf("Elemento desconhecido: %s\n",txt);
-      cin.get();
-      exit(1);
-    }
-  }
-  fclose(arquivo);
-  /* Acrescenta variaveis de corrente acima dos nos, anotando no netlist */
-  nn=nv;
-  for (i=1; i<=ne; i++) {
-    tipo=netlist[i].nome[0];
-    if (tipo=='V' || tipo=='E' || tipo=='F' || tipo=='O') {
-      nv++;
-      if (nv>MAX_NOS) {
-        printf("As correntes extra excederam o numero de variaveis permitido (%d)\n",MAX_NOS);
-        exit(1);
-      }
-      strcpy(lista[nv],"j"); /* Tem espaco para mais dois caracteres */
-      strcat(lista[nv],netlist[i].nome);
-      netlist[i].x=nv;
-    }
-    else if (tipo=='H') {
-      nv=nv+2;
-      if (nv>MAX_NOS) {
-        printf("As correntes extra excederam o numero de variaveis permitido (%d)\n",MAX_NOS);
-        exit(1);
-      }
-      strcpy(lista[nv-1],"jx"); strcat(lista[nv-1],netlist[i].nome);
-      netlist[i].x=nv-1;
-      strcpy(lista[nv],"jy"); strcat(lista[nv],netlist[i].nome);
-      netlist[i].y=nv;
-    }
-  }
-  cin.get();
-  /* Lista tudo */
-  printf("Variaveis internas: \n");
-  for (i=0; i<=nv; i++)
-    printf("%d -> %s\n",i,lista[i]);
-  cin.get();
-  printf("Netlist interno final\n");
-  for (i=1; i<=ne; i++) {
-    tipo=netlist[i].nome[0];
-    if (tipo=='R' || tipo=='I' || tipo=='V') {
-      printf("%s %d %d %g\n",netlist[i].nome,netlist[i].a,netlist[i].b,netlist[i].valor);
-    }
-    else if (tipo=='G' || tipo=='E' || tipo=='F' || tipo=='H') {
-      printf("%s %d %d %d %d %g\n",netlist[i].nome,netlist[i].a,netlist[i].b,netlist[i].c,netlist[i].d,netlist[i].valor);
-    }
-    else if (tipo=='O') {
-      printf("%s %d %d %d %d\n",netlist[i].nome,netlist[i].a,netlist[i].b,netlist[i].c,netlist[i].d);
-    }
-    if (tipo=='V' || tipo=='E' || tipo=='F' || tipo=='O')
-      printf("Corrente jx: %d\n",netlist[i].x);
-    else if (tipo=='H')
-      printf("Correntes jx e jy: %d, %d\n",netlist[i].x,netlist[i].y);
-  }
-  cin.get();
-  /* Monta o sistema nodal modificado */
-  printf("O circuito tem %d nos, %d variaveis e %d elementos\n",nn,nv,ne);
-  cin.get();
-  /* Zera sistema */
-  for (i=0; i<=nv; i++) {
-    for (j=0; j<=nv+1; j++)
-      Yn[i][j]=0;
-  }
-  /* Monta estampas */
-  for (i=1; i<=ne; i++) {
-    tipo=netlist[i].nome[0];
-    if (tipo=='R') {
-      g=1/netlist[i].valor;
-      Yn[netlist[i].a][netlist[i].a]+=g;
-      Yn[netlist[i].b][netlist[i].b]+=g;
-      Yn[netlist[i].a][netlist[i].b]-=g;
-      Yn[netlist[i].b][netlist[i].a]-=g;
-    }
-    else if (tipo=='G') {
-      g=netlist[i].valor;
-      Yn[netlist[i].a][netlist[i].c]+=g;
-      Yn[netlist[i].b][netlist[i].d]+=g;
-      Yn[netlist[i].a][netlist[i].d]-=g;
-      Yn[netlist[i].b][netlist[i].c]-=g;
-    }
-    else if (tipo=='I') {
-      g=netlist[i].valor;
-      Yn[netlist[i].a][nv+1]-=g;
-      Yn[netlist[i].b][nv+1]+=g;
-    }
-    else if (tipo=='V') {
-      Yn[netlist[i].a][netlist[i].x]+=1;
-      Yn[netlist[i].b][netlist[i].x]-=1;
-      Yn[netlist[i].x][netlist[i].a]-=1;
-      Yn[netlist[i].x][netlist[i].b]+=1;
-      Yn[netlist[i].x][nv+1]-=netlist[i].valor;
-    }
-    else if (tipo=='E') {
-      g=netlist[i].valor;
-      Yn[netlist[i].a][netlist[i].x]+=1;
-      Yn[netlist[i].b][netlist[i].x]-=1;
-      Yn[netlist[i].x][netlist[i].a]-=1;
-      Yn[netlist[i].x][netlist[i].b]+=1;
-      Yn[netlist[i].x][netlist[i].c]+=g;
-      Yn[netlist[i].x][netlist[i].d]-=g;
-    }
-    else if (tipo=='F') {
-      g=netlist[i].valor;
-      Yn[netlist[i].a][netlist[i].x]+=g;
-      Yn[netlist[i].b][netlist[i].x]-=g;
-      Yn[netlist[i].c][netlist[i].x]+=1;
-      Yn[netlist[i].d][netlist[i].x]-=1;
-      Yn[netlist[i].x][netlist[i].c]-=1;
-      Yn[netlist[i].x][netlist[i].d]+=1;
-    }
-    else if (tipo=='H') {
-      g=netlist[i].valor;
-      Yn[netlist[i].a][netlist[i].y]+=1;
-      Yn[netlist[i].b][netlist[i].y]-=1;
-      Yn[netlist[i].c][netlist[i].x]+=1;
-      Yn[netlist[i].d][netlist[i].x]-=1;
-      Yn[netlist[i].y][netlist[i].a]-=1;
-      Yn[netlist[i].y][netlist[i].b]+=1;
-      Yn[netlist[i].x][netlist[i].c]-=1;
-      Yn[netlist[i].x][netlist[i].d]+=1;
-      Yn[netlist[i].y][netlist[i].x]+=g;
-    }
-    else if (tipo=='O') {
-      Yn[netlist[i].a][netlist[i].x]+=1;
-      Yn[netlist[i].b][netlist[i].x]-=1;
-      Yn[netlist[i].x][netlist[i].c]+=1;
-      Yn[netlist[i].x][netlist[i].d]-=1;
-    }
-#ifdef DEBUG
-    /* Opcional: Mostra o sistema apos a montagem da estampa */
-    printf("Sistema apos a estampa de %s\n",netlist[i].nome);
-    for (k=1; k<=nv; k++) {
-      for (j=1; j<=nv+1; j++)
-        if (Yn[k][j]!=0) printf("%+3.1f ",Yn[k][j]);
-        else printf(" ... ");
-      printf("\n");
+    fclose(arquivo);
+    /* Acrescenta variaveis de corrente acima dos nos, anotando no netlist */
+    nn=nv;
+    for (i=1; i<=ne; i++) {
+        tipo=netlist[i].nome[0];
+        if (tipo=='V' || tipo=='E' || tipo=='F' || tipo=='O') {
+            nv++;
+            if (nv>MAX_NOS) {
+                printf("As correntes extra excederam o numero de variaveis permitido (%d)\n",MAX_NOS);
+                exit(1);
+            }
+            strcpy(lista[nv],"j"); /* Tem espaco para mais dois caracteres */
+            strcat(lista[nv],netlist[i].nome);
+            netlist[i].x=nv;
+        }
+        else if (tipo=='H') {
+            nv=nv+2;
+            if (nv>MAX_NOS) {
+                printf("As correntes extra excederam o numero de variaveis permitido (%d)\n",MAX_NOS);
+                exit(1);
+            }
+            strcpy(lista[nv-1],"jx"); strcat(lista[nv-1],netlist[i].nome);
+            netlist[i].x=nv-1;
+            strcpy(lista[nv],"jy"); strcat(lista[nv],netlist[i].nome);
+            netlist[i].y=nv;
+        }
     }
     cin.get();
-#endif
-  }
-  /* Resolve o sistema */
-  if (solve(nv, Yn)) {
+    /* Lista tudo */
+    printf("Variaveis internas: \n");
+    for (i=0; i<=nv; i++)
+        printf("%d -> %s\n",i,lista[i]);
     cin.get();
-    exit(0);
-  }
-#ifdef DEBUG
-  /* Opcional: Mostra o sistema resolvido */
-  printf("Sistema resolvido:\n");
-  for (i=1; i<=nv; i++) {
-      for (j=1; j<=nv+1; j++)
-        if (Yn[i][j]!=0) printf("%+3.1f ",Yn[i][j]);
-        else printf(" ... ");
-      printf("\n");
+    printf("Netlist interno final\n");
+    for (i=1; i<=ne; i++) {
+        tipo=netlist[i].nome[0];
+        if (tipo=='R' || tipo=='I' || tipo=='V') {
+            printf("%s %d %d %g\n",netlist[i].nome,netlist[i].a,netlist[i].b,netlist[i].valor);
+        }
+        else if (tipo=='G' || tipo=='E' || tipo=='F' || tipo=='H') {
+            printf("%s %d %d %d %d %g\n",netlist[i].nome,netlist[i].a,netlist[i].b,netlist[i].c,netlist[i].d,netlist[i].valor);
+        }
+        else if (tipo=='O') {
+            printf("%s %d %d %d %d\n",netlist[i].nome,netlist[i].a,netlist[i].b,netlist[i].c,netlist[i].d);
+        }
+        if (tipo=='V' || tipo=='E' || tipo=='F' || tipo=='O')
+            printf("Corrente jx: %d\n",netlist[i].x);
+        else if (tipo=='H')
+            printf("Correntes jx e jy: %d, %d\n",netlist[i].x,netlist[i].y);
     }
-  cin.get();
+    cin.get();
+    /* Monta o sistema nodal modificado */
+    printf("O circuito tem %d nos, %d variaveis e %d elementos\n",nn,nv,ne);
+    cin.get();
+    /* Zera sistema */
+    for (i=0; i<=nv; i++) {
+        for (j=0; j<=nv+1; j++)
+            Yn[i][j]=0;
+    }
+    /* Monta estampas */
+    for (i=1; i<=ne; i++) {
+        tipo=netlist[i].nome[0];
+        if (tipo=='R') {
+            g=1/netlist[i].valor;
+            Yn[netlist[i].a][netlist[i].a]+=g;
+            Yn[netlist[i].b][netlist[i].b]+=g;
+            Yn[netlist[i].a][netlist[i].b]-=g;
+            Yn[netlist[i].b][netlist[i].a]-=g;
+        }
+        else if (tipo=='G') {
+            g=netlist[i].valor;
+            Yn[netlist[i].a][netlist[i].c]+=g;
+            Yn[netlist[i].b][netlist[i].d]+=g;
+            Yn[netlist[i].a][netlist[i].d]-=g;
+            Yn[netlist[i].b][netlist[i].c]-=g;
+        }
+        else if (tipo=='I') {
+            g=netlist[i].valor;
+            Yn[netlist[i].a][nv+1]-=g;
+            Yn[netlist[i].b][nv+1]+=g;
+        }
+        else if (tipo=='V') {
+            Yn[netlist[i].a][netlist[i].x]+=1;
+            Yn[netlist[i].b][netlist[i].x]-=1;
+            Yn[netlist[i].x][netlist[i].a]-=1;
+            Yn[netlist[i].x][netlist[i].b]+=1;
+            Yn[netlist[i].x][nv+1]-=netlist[i].valor;
+        }
+        else if (tipo=='E') {
+            g=netlist[i].valor;
+            Yn[netlist[i].a][netlist[i].x]+=1;
+            Yn[netlist[i].b][netlist[i].x]-=1;
+            Yn[netlist[i].x][netlist[i].a]-=1;
+            Yn[netlist[i].x][netlist[i].b]+=1;
+            Yn[netlist[i].x][netlist[i].c]+=g;
+            Yn[netlist[i].x][netlist[i].d]-=g;
+        }
+        else if (tipo=='F') {
+            g=netlist[i].valor;
+            Yn[netlist[i].a][netlist[i].x]+=g;
+            Yn[netlist[i].b][netlist[i].x]-=g;
+            Yn[netlist[i].c][netlist[i].x]+=1;
+            Yn[netlist[i].d][netlist[i].x]-=1;
+            Yn[netlist[i].x][netlist[i].c]-=1;
+            Yn[netlist[i].x][netlist[i].d]+=1;
+        }
+        else if (tipo=='H') {
+            g=netlist[i].valor;
+            Yn[netlist[i].a][netlist[i].y]+=1;
+            Yn[netlist[i].b][netlist[i].y]-=1;
+            Yn[netlist[i].c][netlist[i].x]+=1;
+            Yn[netlist[i].d][netlist[i].x]-=1;
+            Yn[netlist[i].y][netlist[i].a]-=1;
+            Yn[netlist[i].y][netlist[i].b]+=1;
+            Yn[netlist[i].x][netlist[i].c]-=1;
+            Yn[netlist[i].x][netlist[i].d]+=1;
+            Yn[netlist[i].y][netlist[i].x]+=g;
+        }
+        else if (tipo=='O') {
+            Yn[netlist[i].a][netlist[i].x]+=1;
+            Yn[netlist[i].b][netlist[i].x]-=1;
+            Yn[netlist[i].x][netlist[i].c]+=1;
+            Yn[netlist[i].x][netlist[i].d]-=1;
+        }
+#ifdef DEBUG
+        /* Opcional: Mostra o sistema apos a montagem da estampa */
+        printf("Sistema apos a estampa de %s\n",netlist[i].nome);
+        for (k=1; k<=nv; k++) {
+            for (j=1; j<=nv+1; j++)
+                if (Yn[k][j]!=0) printf("%+3.1f ",Yn[k][j]);
+                else printf(" ... ");
+            printf("\n");
+        }
+        cin.get();
 #endif
-  /* Mostra solucao */
-  printf("Solucao:\n");
-  strcpy(txt,"Tensao");
-  for (i=1; i<=nv; i++) {
-    if (i==nn+1) strcpy(txt,"Corrente");
-    printf("%s %s: %g\n",txt,lista[i],Yn[i][nv+1]);
-  }
-  cin.get();
+    }
+    /* Resolve o sistema */
+    if (solve(nv, Yn)) {
+        cin.get();
+        exit(0);
+    }
+#ifdef DEBUG
+    /* Opcional: Mostra o sistema resolvido */
+    printf("Sistema resolvido:\n");
+    for (i=1; i<=nv; i++) {
+            for (j=1; j<=nv+1; j++)
+                if (Yn[i][j]!=0) printf("%+3.1f ",Yn[i][j]);
+                else printf(" ... ");
+            printf("\n");
+        }
+    cin.get();
+#endif
+    /* Mostra solucao */
+    printf("Solucao:\n");
+    strcpy(txt,"Tensao");
+    for (i=1; i<=nv; i++) {
+        if (i==nn+1) strcpy(txt,"Corrente");
+        printf("%s %s: %g\n",txt,lista[i],Yn[i][nv+1]);
+    }
+    cin.get();
 }
 
