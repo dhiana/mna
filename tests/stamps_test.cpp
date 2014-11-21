8c4dc5db8ae87cae2d91076ba025828356709772
Dhiana Deva <dhiana.deva@gmail.com>
2014-11-21 18:59:10 -0200

    Adds test of resistor stamp
    Attention: As is, netlist must not start from 0.
    
    This is a serious coupling between the way the netlist is read and how
    the final matrix should be...
    
    To be improved!!!

---
+++ tests/stamps_test.cpp
@@ -0,0 +1,37 @@
+#include "circuits/circuit.h"
+#include "matrix/matrix.h"
+#include "gtest/gtest.h"
+#include <iostream>
+#include <stdio.h>
+
+
+using namespace std;
+
+
+TEST(CircuitStampsTest, SimpleResistor) {
+    // Arrange
+    int numElements = 1;
+    int numVariables = 2;
+    double matrix[MAX_NOS+1][MAX_NOS+2];
+    init(numVariables, matrix);
+    Element resistor;
+    resistor.nome = "R1";
+    resistor.a = 1;
+    resistor.b = 2;
+    resistor.valor = 2;
+    vector<Element> netlist(2);
+    netlist[1] = resistor;
+    // Act
+    applyStamps(numElements, numVariables, netlist, matrix);
+    // Assert
+    double expected[MAX_NOS+1][MAX_NOS+2] = {
+        {0,    0,    0, 0},
+        {0,  0.5, -0.5, 0},
+        {0, -0.5,  0.5, 0}
+    };
+    for (int i=1; i<=numVariables; i++) {
+        for (int j=1; j<=numVariables+1; j++) {
+            EXPECT_EQ(expected[i][j], matrix[i][j]);
+        }
+    }
+}
