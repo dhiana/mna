6b3e1e69b974e77eea4588a9d868b50675fe4183
Felipe De Leo <felipedeleo@gmail.com>
2014-11-22 18:38:20 -0200

    Fixed Identing Problems

--- src/circuits/circuit.cpp
+++ src/circuits/circuit.cpp
@@ -127,37 +127,36 @@ void printSolution(int numVariables, int numNodes, double Yn[MAX_NODES+1][MAX_NO
 /* Function to write the Solution into an Output File */
 
 bool WriteSolutionToFile(string filename, int numVariables, int numNodes, double Yn[MAX_NODES+1][MAX_NODES+2], vector<string> lista){
-	// Opening the File for Writing
-	ofstream file(filename.c_str(), ofstream::out);
+    // Opening the File for Writing
+    ofstream file(filename.c_str(), ofstream::out);
 
-	/* Writing the Header */
-	for (int i=0; i<=numVariables; i++){
-		if (i==0)
-			file << "t ";
-		// The Nodal Tensions
-		else if (i<=numNodes)
-			file << lista[i] << " ";
-		// The currents
-		else if (i>numNodes){
-			file << lista[i] << " ";
-			// TERMINAR PARA ADAPTAR SÓ O NOME DA VARIÁVEL => EXEMPLO => jO1
-		}
-	}
-	file << endl;
-	/* End of Header */
+    /* Writing the Header */
+    for (int i = 0; i <= numVariables; i++){
+        if (i == 0)
+            file << "t ";
+        // The Nodal Tensions
+        else if (i <= numNodes)
+            file << lista[i] << " ";
+        // The currents
+        else if (i > numNodes){
+            file << lista[i] << " ";
+        }
+    }
+    file << endl;
+    /* End of Header */
 
-	/* Start of Values Writing */
-	for (int i=0; i<numVariables; i++){
-		if (i==0)
-			file << "0 ";
-		else if (i>0)
-			file << Yn[i][i] << " ";
-	}
-	file << std::endl;
+    /* Start of Values Writing */
+    for (int i = 0; i < numVariables; i++){
+        if (i == 0)
+            file << "0 ";
+        else if (i>0)
+            file << Yn[i][i] << " ";
+    }
+    file << std::endl;
 
-	/* Finish of Values Writing */
+    /* Finish of Values Writing */
 
-	//Closing The File
-	file.close();
-	return true;
+    //Closing The File
+    file.close();
+    return true;
 }
