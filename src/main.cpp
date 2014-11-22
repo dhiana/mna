7f9085bf0040ebd449c40ebc5a2ba181927cdea4
Dhiana Deva <dhiana.deva@gmail.com>
2014-11-22 20:34:03 -0200

    Moves utils functions to separate file
    Moved:
    exitPolitely
    readNetlistFile
    printIntro

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "consts.h"
+#include "utils.h"
 #include "circuits/circuit.h"
 #include "circuits/element.h"
 #include "matrix/matrix.h"
@@ -11,49 +12,6 @@
 #include <cstdlib>
 
 using namespace std;
-
-
-void exitPolitely(int exitCode) {
-	#if defined (WIN32) || defined(_WIN32)
-	cout << endl << "Press any key to exit...";
-	cin.get();
-	cin.get();
-	#endif
-	exit(exitCode);
-}
-
-
-int readNetlistFile(int argc, char** argv, ifstream& netlistFile){
-    string filepath;
-
-    switch(argc) {
-        case 1: {
-            cout << "Enter path to netlist file: ";
-            cin >> filepath;
-            break;
-        }
-        case 2: {
-            filepath = argv[1];
-            break;
-        }
-        default:
-            cerr << "FAILURE: Too much information!" << endl;
-            return EXIT_FAILURE;
-    }
-
-    netlistFile.open(filepath.c_str(), ifstream::in);
-    if(!netlistFile.is_open()){
-        cerr << "FAILURE: Cannot open file " << filepath << endl;
-		return EXIT_FAILURE;
-    }
-    return 0;
-}
-
-void printIntro(){
-    cout << endl << "Modified Nodal Analysis"
-         << endl << "Originally by Antonio Carlos M. de Queiroz (acmq@coe.ufrj.br)"
-         << endl << "Modified by Dhiana Deva, Felipe de Leo and Silvino Vieira" << endl << endl;
-}
 
 
 /* The program starts here */
