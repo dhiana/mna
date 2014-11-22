3752b4a57698cb895c56ac1f52ecec3efcdde30c
Dhiana Deva <dhiana.deva@gmail.com>
2014-11-22 18:38:27 -0200

    Updates refactoring of nv as numVariables

--- src/circuits/element.cpp
+++ src/circuits/element.cpp
@@ -12,7 +12,7 @@ Element::Element()
 {
 }
 
-Element::Element(string netlistLine, int &numElements,  int &nv, vector<string> &list)
+Element::Element(string netlistLine, int &numElements,  int &numVariables, vector<string> &list)
 {
     stringstream sstream(netlistLine);
     char na[MAX_NAME], nb[MAX_NAME], nc[MAX_NAME], nd[MAX_NAME];
@@ -23,25 +23,25 @@ Element::Element(string netlistLine, int &numElements,  int &nv, vector<string> 
     if (type=='R' || type=='I' || type=='V') {
         sstream >> na >> nb >> valor;
         cout << name << " " << na << " " << nb << " " << valor << endl;
-        a = number(na, nv, list);
-        b = number(nb, nv, list);
+        a = number(na, numVariables, list);
+        b = number(nb, numVariables, list);
     }
     else if (type=='G' || type=='E' || type=='F' || type=='H') {
         sstream >> na >> nb >> nc >> nd >> valor;
         cout << name << " " << na << " " << nb << " " << nc << " "
              << nd << " "<< valor << endl;
-        a = number(na, nv, list);
-        b = number(nb, nv, list);
-        c = number(nc, nv, list);
-        d = number(nd, nv, list);
+        a = number(na, numVariables, list);
+        b = number(nb, numVariables, list);
+        c = number(nc, numVariables, list);
+        d = number(nd, numVariables, list);
     }
     else if (type=='O') {
         sstream >> na >> nb >> nc >> nd;
         cout << name << " " << na << " " << nb << " " << nc << " " << nd << " " << endl;
-        a = number(na, nv, list);
-        b = number(nb, nv, list);
-        c = number(nc, nv, list);
-        d = number(nd, nv, list);
+        a = number(na, numVariables, list);
+        b = number(nb, numVariables, list);
+        c = number(nc, numVariables, list);
+        d = number(nd, numVariables, list);
     }
     else if (type=='*') { /* Comentario comeca com "*" */
         cout << "Comentario: " << netlistLine;
@@ -70,23 +70,23 @@ Element::Element(string name, double value, int a, int b, int c, int d, int x, i
 }
 
 /** Rotina que conta os nos e atribui Element::numbers a eles */
-int Element::number(const char *name, int &nv, vector<string> &list)
+int Element::number(const char *name, int &numVariables, vector<string> &list)
 {
     int i=0, achou=0;
 
-    while (!achou && i<=nv)
+    while (!achou && i<=numVariables)
         if (!(achou=!list[i].compare(name))) i++;
     if (!achou) {
-        if (nv==MAX_NODES) {
-            cout << "O programa so aceita ate " << nv <<  " nos" << endl;
+        if (numVariables==MAX_NODES) {
+            cout << "O programa so aceita ate " << numVariables <<  " nos" << endl;
             #if defined (WIN32) || defined(_WIN32)
             cin.get();
             #endif
             exit(EXIT_FAILURE);
         }
-        nv++;
-        list[nv] = name;
-        return nv; /* novo no */
+        numVariables++;
+        list[numVariables] = name;
+        return numVariables; /* novo no */
     }
     else {
         return i; /* no ja conhecido */
