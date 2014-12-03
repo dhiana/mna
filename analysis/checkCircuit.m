8368cb3015b13183f3ca6593c587e3995b314882
Dhiana Deva <dhiana.deva@gmail.com>
2014-12-03 03:41:52 -0200

    Extracts analysis for single circuit

---
+++ analysis/checkCircuit.m
@@ -0,0 +1,28 @@
+function checkCircuit(circuit)
+    mnapoly_filename = strcat('../data/', circuit,'_mnapoly.tab');
+    mnae_filename = strcat('../results/', circuit,'.tab');
+    fprintf('Loading files...\n');
+    mnapoly = importdata(mnapoly_filename);
+    mnae = importdata(mnae_filename);
+    fprintf('Files loaded...\n');
+    if isstruct(mnae) && isstruct(mnapoly)
+        endRows = min(size(mnapoly.data,1),size(mnae.data,1));
+        if endRows
+            diff = mnae.data(1:endRows,:) - mnapoly.data(1:endRows,:);
+            f = figure;
+            subplot(1,2,1);
+            plot(diff');
+            title(circuit);
+            xlabel('Variable');
+            ylabel('Difference w.r.t MNAE');
+            subplot(1,2,2);
+            plot(diff);
+            title(circuit);
+            xlabel('Time');
+            ylabel('Difference w.r.t MNAE');
+            saveas(f, strcat(circuit,'.png'), 'png');
+        else
+            fprintf('There wasnt a full output...\n');
+        end
+    end
+end
