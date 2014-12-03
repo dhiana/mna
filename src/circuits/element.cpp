baaf1e30cbfc90a0bc8f972a76f9e74636e16a61
Dhiana Deva <dhiana.deva@gmail.com>
2014-12-03 09:45:29 -0200

    Revert "Addition of K (inductor coupling) as element"
    This reverts commit ef551940bba8187aa406ea2f2a02fbec64e77c4b.

--- src/circuits/element.cpp
+++ src/circuits/element.cpp
@@ -107,14 +107,6 @@ Element::Element(string netlistLine,
         a = getNodeNumber(na, numNodes, variablesList);
         b = getNodeNumber(nb, numNodes, variablesList);
     }
-    /* ADAPTAR DEFINIÇÃO PARA K = PENDENTE
-    else if (type == 'K') {
-        sstream >> na >> nb >> value;
-        cout << na << " " << nb << " " << value << " " << endl;
-
-        x = getNodeNumber(na, numNodes, variablesList); //get Outra coisa aqui pra descobrir qual o L1 e qual o L2
-        y = getNodeNumber(nb, numNodes, variablesList);
-    } */
 }
 
 void Element::addCurrentVariables(int &numVariables, vector<string> &variablesList){
@@ -143,14 +135,6 @@ void Element::addCurrentVariables(int &numVariables, vector<string> &variablesLi
         x = numVariables;
         variablesList[numVariables] = "j" + name;
     }
-    /* ADAPTAR DEFINIÇÃO PARA K = PENDENTE
-    else if (type == 'K') {
-        numVariables = numVariables + 2;
-        x = numVariables - 1;
-        y = numVariables;
-        variablesList[numVariables - 1] = "jx" + name;
-        variablesList[numVariables] = "jy" + name;
-    } */
 }
 
 Element::Element(string name,
@@ -420,25 +404,6 @@ void Element::applyStamp(double Yn[MAX_NODES+1][MAX_NODES+2],
             Yn[x][numVariables+1] += V0;
         }
     }
-    else if (type == 'K'){        
-        if (!t){
-            double R = TOLG;        
-            Yn[x][y] += R;
-            Yn[y][x] += R;
-        }
-        else{
-            double M = (value / step);
-            double jx = lastStepSolution[x];
-            double jy = lastStepSolution[y];
-            double Vx = M*jy;
-            double Vy = M*jx;
-
-            Yn[x][y] += M;
-            Yn[y][x] += M;
-            Yn[x][numVariables + 1] += Vx;
-            Yn[y][numVariables + 1] += Vy;
-        }
-    }
 }
 
 
@@ -471,7 +436,7 @@ int Element::getNodeNumber(const char *name,
 bool Element::isValidElement(const char &netlistLinePrefix){
     // Initializing set with tmpElementPrefixes
     char tmpElementPrefixes[] = {
-        'R', 'I', 'V', 'G', 'E', 'F', 'H', 'O', 'L', 'C', 'K'
+        'R', 'I', 'V', 'G', 'E', 'F', 'H', 'O', 'L', 'C'
     };
     set<char> elementPrefixes(
         tmpElementPrefixes,
