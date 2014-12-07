9f96df221659ecaab82e74bf6dea02dbe3bd98b2
dhiana <dhiana.deva@gmail.com>
2014-12-07 10:43:40 -0200

    Raises Newton Raphson max loops and attempts

--- src/matrix/newtonraphson.cpp
+++ src/matrix/newtonraphson.cpp
@@ -9,8 +9,8 @@
 
 using namespace std;
 
-static const int MAX_ATTEMPTS = 50;
-static const int MAX_LOOPS = 100;
+static const int MAX_ATTEMPTS = 500;
+static const int MAX_LOOPS = 1000;
 static const double TOLERANCE = 1e-4;
 
 
